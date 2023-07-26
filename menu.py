f51fd85186649bf9481834c7320e96db956de5a0
Jared <73363096+jaredonnelly@users.noreply.github.com>
2023-07-25 23:49:38 -0400

    Update menu.py
    -added display_list function for supporting new user selected list parameters.
    -updated request_validation to option validation to better reflect it's role
    -updated option_validation to be more abstract and usable
    -added correct_characters class to help seperate the correct characters to use for each of the menus

--- menu.py
+++ menu.py
@@ -11,50 +11,100 @@ def display():
 
     return input("Enter an option: ")
 
-def request_validation(option):
+def display_list():
+    ref = correct_characters()
+    count = 'w'
+    while count == 'w':
+        print("\nWould you like to view all books or just the select few?")
+        print("a - print all books")
+        print("c - print selection of books\n")
+        count = option_validation(input("Enter an option: "), [ref.list_func, ref.list_func_menu])
+
+    if count == 'c':
+        count = 'w'
+        while count == 'w':
+            count = int_validation(str(input("Enter amount: ")))
+
+    sortby = 'w'
+    while sortby == 'w':    
+        print("\nSelect how you would like to sort the list and then enter how many you'd like to see.")
+        print("i - sort by isbn.")
+        print("t - sort by title.")
+        print("a - sort by authors name.")
+        print("p - sort by publishers name.")
+        print("d - sort by publication date (year).")
+        print("s - stock, the amount of books in inventory.\n")
+        sortby = option_validation(input("Enter an option: "), [ref.list_c, ref.list_c_menu])
+
+    match sortby:
+        case 'i':
+            sortby = "ISBN"
+        case 't':
+            sortby = "TITLE"
+        case 'a':
+            sortby = "AUTHOR"
+        case 'p':
+            sortby = "PUBLISHER"
+        case 'd':
+            sortby = "YEAR"
+        case 's':
+            sortby = "STOCK"
+
+    return sortby, count
+
+def option_validation(option, menu):
     """Validates option that was inputed by user"""
 
-    if type(option) != str or len(option) != 1:
-        print("Invalid input. Inputs must be a single character a, r, s, l, or q.\n")
+    if type(option) != str or len(option) != 1 or not option.isalpha():
+        print("Invalid input. Inputs must be a single alphabetical character: ", menu[1], "\n")
         return "w"
     
-    match option.lower():
-        case "a":
-            return "a"
-        case "r":
-            return "r"
-        case "s":
-            return "s"
-        case "l":
-            return "l"
-        case "q":
-            print("Thank you for using our book manager. Come again soon!\n")
-            return "q"
-        case _:
-            print("Invalid input. Inputs must be a single character a, r, s, or l.\n")
-            return "w"
+    if option.lower() in menu[0]:
+        return option.lower()
+    else:
+        print("Invalid input. Please select an option from the menu below.")
+        return "w" # w is a reserved character and stands for 'wrong'.
+
+def int_validation(input_int):
+    """returns a validated int or false"""
+    if input_int.isnumeric():
+        return int(input_int)
+    else:
+        print("The number inputted must be a positive, whole, integer.")
+        return 'w'
+
+class correct_characters():
+    main = ['a', 'r', 's', 'l', 'q']
+    main_menu = "a, r, s, l, and q."
+
+    list_func = ['a', 'c']
+    list_func_menu = "a or c."
+
+    list_c = ['i', 't', 'a', 'p', 'd', 's']
+    list_c_menu = ['i, t, a, p, d, or s.']
+
 
 class TestRequestValidation(unittest.TestCase):
     def test_valid_lowercase(self):
-        self.assertEqual(request_validation('a'), 'a')
-        self.assertEqual(request_validation('r'), 'r')
-        self.assertEqual(request_validation('s'), 's')
-        self.assertEqual(request_validation('l'), 'l')
-        self.assertEqual(request_validation('q'), 'q')
+        self.assertEqual(option_validation('a'), 'a')
+        self.assertEqual(option_validation('r'), 'r')
+        self.assertEqual(option_validation('s'), 's')
+        self.assertEqual(option_validation('l'), 'l')
+        self.assertEqual(option_validation('q'), 'q')
 
     def test_valid_uppercase(self):
-        self.assertEqual(request_validation('A'), 'a')
-        self.assertEqual(request_validation('R'), 'r')
-        self.assertEqual(request_validation('S'), 's')
-        self.assertEqual(request_validation('L'), 'l')
-        self.assertEqual(request_validation('Q'), 'q')
+        self.assertEqual(option_validation('A'), 'a')
+        self.assertEqual(option_validation('R'), 'r')
+        self.assertEqual(option_validation('S'), 's')
+        self.assertEqual(option_validation('L'), 'l')
+        self.assertEqual(option_validation('Q'), 'q')
 
     def test_invalid(self):
-        self.assertEqual(request_validation(123), 'w')
-        self.assertEqual(request_validation(''), 'w')
-        self.assertEqual(request_validation([1,'a']), 'w')
-        self.assertEqual(request_validation('ar'), 'w')
-        self.assertEqual(request_validation('w'), 'w')
+        self.assertEqual(option_validation(123), 'w')
+        self.assertEqual(option_validation(''), 'w')
+        self.assertEqual(option_validation([1,'a']), 'w')
+        self.assertEqual(option_validation('ar'), 'w')
+        self.assertEqual(option_validation('w'), 'w')
 
 
 if __name__ == '__main__':
